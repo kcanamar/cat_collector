27b18a57c1f474c20121e524d62e442096233ae8
kcanamar <99228411+kcanamar@users.noreply.github.com>
2024-03-22 10:48:50 -0700

    wip: starting point

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path("about/", views.about, name="about"),
+    # route for cats index
+    path('cats/', views.cat_index, name="index"),
 ]
