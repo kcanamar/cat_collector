0d87e1447b57607870a8d19956a9f4b1b6508348
kcanamar <99228411+kcanamar@users.noreply.github.com>
2024-03-22 10:25:48 -0700

    wip: base cat_collector app setup

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Add the following import
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('<h1>Hello Cats</h1>')
+
+def about(request):
+    return render(request, 'about.html')
